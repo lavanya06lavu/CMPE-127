8592c987787c0d57e2a304aed1218bf96e6132fa
lavanya06lavu <34131408+lavanya06lavu@users.noreply.github.com>
2024-01-29 13:26:19 -0800

    Create test.asm

---
+++ test.asm
@@ -0,0 +1,20 @@
+;add two 32 integers
+
+.386
+.model flat, stdcall
+.stack 4096
+ExitProcess proto,dwExitCode:dword
+
+.data
+
+sum DWORD ?
+
+.code
+main proc
+	mov eax, 7
+	add eax, 4
+	mov sum, eax
+	invoke ExitProcess, 0
+
+main endp
+end main
